97d7927bf7218e8e37e54dcde80a7f306fbca198
Surain <surain.wira@gmail.com>
2023-06-19 01:54:44 +0800

    created newletter section

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -7,19 +7,39 @@ height: 60vh;
 background-color:#fcf5f5;
 display: flex; 
 align-items: center; 
-justify-content:center;`;
-const Title = styled.h1``;
-const Description = styled.div``;
-const InputContainer = styled.div``;
-const Input = styled.input``;
-const Button = styled.button``;
+justify-content:center;
+flex-direction: column;`;
+const Title = styled.h1`
+font-size:70px;
+margin-bottom: 20px;`;
+const Description = styled.div`
+font-size: 24px;
+font-weight: 300;
+margin-bottom: 20px;`;
+const InputContainer = styled.div`
+width: 50%;
+height: 40px;
+background-color: white;
+display:flex;
+justify-content:space-between;
+border: 1px solid lightgray;`;
+const Input = styled.input`
+border: none;
+flex:8;
+padding-left:20px;
+color: grey;`;
+const Button = styled.button`
+flex: 1;
+border: none;
+background-color: teal;
+color:white;`;
 export const Newsletter = () => {
     return (
         <Container>
             <Title>Newletter</Title>
             <Description>Get our latest update on clothing we are about to release. Ofcourse it comes with great and worthy discounts as well.</Description>
             <InputContainer>
-                <Input />
+                <Input value={"Your Email"} />
                 <Button><Send /></Button>
             </InputContainer>
         </Container>
