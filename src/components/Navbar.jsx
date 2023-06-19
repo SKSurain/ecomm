db07e286abffd660a2e7157665a0ae709d75fa93
Surain <surain.wira@gmail.com>
2023-06-20 07:11:11 +0800

    changed padding in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,10 +4,11 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-height: 60px;`
+height: 60px;
+padding: 0px 20px 10px 20px;
+`
 
 const Wrapper = styled.div`
-padding: 10px 20px;
 display: flex;
 align-items: center;
 justify-content: space-between
