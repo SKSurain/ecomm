108e4dd70f3823b768aa2d61ac77752fadf9fe76
Surain <surain.wira@gmail.com>
2023-06-20 07:13:23 +0800

    created product list page

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -7,25 +7,58 @@ import { Newsletter } from '../components/Newsletter'
 import { Footer } from '../components/Footer'
 
 const Container = styled.div``;
-const Title = styled.h1``;
+const Title = styled.h1`
+margin:20px;`;
 const FilterContainer = styled.div`
 display:flex;
 justify-content:space-between;
 margin:20px; `;
 const Filter = styled.div`
-margin: 20px;`;
-const FilterText = styled.div``;
+margin: 20px;
+display:flex;`;
+const FilterText = styled.div`
+font-size: 20px;
+font-weight: 600;
+margin-right: 20px;`;
+const Select = styled.select`
+padding: 10px;
+margin-right: 20px;`;
+const Option = styled.option``;
 
 
 export const ProductList = () => {
     return (
         <Container>
-            <Announcement />
             <Navbar />
+            <Announcement />
             <Title>Jackets</Title>
             <FilterContainer>
-                <Filter>Filter 1</Filter>
-                <Filter>Filter 2</Filter>
+                <Filter>
+                    <FilterText>Filter Products:</FilterText>
+                    <Select>
+                        <Option disabled selected>Color</Option>
+                        <Option>White</Option>
+                        <Option>Black</Option>
+                        <Option>Red</Option>
+                        <Option>Yellow</Option>
+                        <Option>Green</Option>
+                    </Select>
+                    <Select>
+                        <Option disabled selected>Size</Option>
+                        <Option>XS</Option>
+                        <Option>S</Option>
+                        <Option>M</Option>
+                        <Option>L</Option>
+                        <Option>XL</Option>
+                    </Select>
+                </Filter>
+
+                <Filter><FilterText >Sort Products:</FilterText>
+                    <Select>
+                        <Option selected>Newest</Option>
+                        <Option>Price (asc)</Option>
+                        <Option>Newest (desc)</Option>
+                    </Select></Filter>
             </FilterContainer>
             <Products />
             <Newsletter />
