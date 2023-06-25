31cffc3e297a21a282fc32bf578cb3f596cc9db2
Surain <surain.wira@gmail.com>
2023-06-25 10:52:30 +0800

    added flexible routing for register and login page

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import { Product } from './pages/Product'
 import { Register } from './pages/Register'
 import { Login } from './pages/Login'
 import { Cart } from './pages/Cart'
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Homepage from './pages/Home';
 
 
 
@@ -15,6 +16,9 @@ body {
   font-family: 'Urbanist',sans-serif;
 }`
 function App() {
+
+  const user = true;
+
   return (
     <>
       <GlobalStyle />
@@ -22,8 +26,11 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/products" element={<ProductList />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
+          <Route path="/products/:category" element={<ProductList />} />
+          <Route path="/register" element=
+            {user ? <Navigate to="/" /> : <Register />} />
+          <Route path="/login" element=
+            {user ? <Navigate to="/" /> : <Login />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/product" element={<Product />} />
         </Routes>
