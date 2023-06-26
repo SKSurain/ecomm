2b41d798ca9c148982e04b4ad80025851599f1d4
Surain <surain.wira@gmail.com>
2023-06-27 01:03:52 +0800

    added state for user and cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,36 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { createStore } from 'redux'
+import { configureStore, getDefaultMiddleware, combineReducers } from '@reduxjs/toolkit'
+import {
+    persistStore,
+    persistReducer,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from 'redux-persist'
 import cartReducer from "./cartRedux"
+import userReducer from './userRedux'
+import storage from 'redux-persist/lib/storage'
 
-export default configureStore({
-    reducer: {
-        cart: cartReducer,
-    }
-})
+const persistConfig = {
+    key: 'root',
+    version: 1,
+    storage,
+}
+
+const rootReducer = combineReducers({ user: userReducer, cart: cartReducer })
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+            },
+        }),
+});
+
+export let persistor = persistStore(store);
