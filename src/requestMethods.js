9ce2210e5ef6e3d39cc2c3924e8b8c69c8b716c9
Surain <surain.wira@gmail.com>
2023-07-02 16:05:31 +0800

    updated user request function

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,32 +1,14 @@
-import axios from 'axios';
+import axios from 'axios'
 
 const BASE_URL = "http://localhost:5000/api/";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user).currentUser.accessToken;
 
-// Function to get the token from local storage
-const getToken = () => {
-    const root = localStorage.getItem("persist:root");
-    if (root) {
-        const parsedRoot = JSON.parse(JSON.parse(root));
-        if (parsedRoot.user && parsedRoot.user.currentUser) {
-            return parsedRoot.user.currentUser.accessToken;
-        }
-    }
-    return null;
-}
-
+console.log(TOKEN)
 export const publicRequest = axios.create({
     baseURL: BASE_URL,
-});
+})
 
-// Function to get axios instance with attached token
-export const userRequest = () => {
-    const TOKEN = getToken();
-    if (TOKEN) {
-        return axios.create({
-            baseURL: BASE_URL,
-            headers: { token: `Bearer ${TOKEN}` },
-        });
-    }
-    // Handle case when there is no token
-    throw new Error("User is not logged in");
-}
+export const userRequest = axios.create({
+    baseURL: BASE_URL,
+    headers: { token: `Bearer ${TOKEN}` },
+})
